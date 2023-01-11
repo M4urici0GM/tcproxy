513c0cdf63b1f11df3d71879d3e69762c2ad7129
Mauricio Gomes Barbosa <mgbftw@gmail.com>
2023-01-10 21:50:40 -0300

    added more tests for underlying core code

--- tcproxy-core/src/lib.rs
+++ tcproxy-core/src/lib.rs
@@ -16,6 +16,21 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 
 pub mod test_util {
+
+    pub fn generate_random_buffer(buffer_size: i32) -> bytes::BytesMut {
+        use bytes::{BufMut, BytesMut};
+
+        let initial_vec: Vec<u8> = vec![];
+        let result = (0..buffer_size)
+            .map(|_| rand::random::<u8>())
+            .fold(initial_vec, |mut a, b| {
+                a.put_u8(b);
+                a
+            });
+
+        BytesMut::from(result.as_slice())
+    }
+
     pub mod macros {
         #[macro_export]
         macro_rules! is_type {
